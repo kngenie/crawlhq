39f7c48427dfcdc151ecb69ee1a702fbb949f9ca
kenji <kenji@d74894b3-2683-4383-ad00-54612c9f1af1>
2011-07-13 22:14:45 +0000

    leveldb module: more methods for IntHash (delete, close, closed, repair_db) and iterator implementation (barely support forward iteration only).
    git-svn-id: svn+ssh://home.us.archive.org/srv/svn/widecrawl/trunk/hq@269 d74894b3-2683-4383-ad00-54612c9f1af1

--- cext/leveldb.cpp
+++ cext/leveldb.cpp
@@ -31,19 +31,67 @@ public:
   void FindShortSuccessor(string*) const { }
 };
 
-typedef struct PyLevelDBIntHash : public PyObject {
+typedef struct PyLevelDB : public PyObject {
   //PyObject_HEAD
   //
   leveldb::DB *db;
 
+  // these functions have no corresponding C API
+  static PyObject *close(PyObject *self, PyObject *args);
+  static PyObject *closed(PyObject *self, PyObject *args);
+} PyLevelDB;
+
+typedef struct PyLevelDBIntHash : public PyLevelDB {
   static int init(PyObject *self, PyObject *args, PyObject *kwds);
   static void dealloc(PyObject *self);
   static PyObject *put(PyObject *self, PyObject *args);
   static PyObject *get(PyObject *self, PyObject *args);
+  static PyObject *Delete(PyObject *self, PyObject *args);
+  static PyObject *new_iterator(PyObject *self, PyObject *args,
+				PyObject *kwds);
+
+  static PyObject *repair_db(PyObject *cls, PyObject *args, PyObject *kwds);
 } PyLevelDBIntHash;
+
+typedef struct PyLevelDBIterator : public PyObject {
+  leveldb::Iterator *it;
+
+  static void dealloc(PyObject *self);
+  static PyObject *valid(PyObject *self, PyObject *args);
+  static PyObject *seek_to_first(PyObject *self, PyObject *args);
+  static PyObject *seek_to_last(PyObject *self, PyObject *args);
+  static PyObject *seek(PyObject *self, PyObject *args);
+  static PyObject *next(PyObject *self, PyObject *args);
+  static PyObject *prev(PyObject *self, PyObject *args);
+  static PyObject *key(PyObject *self, PyObject *args);
+  static PyObject *value(PyObject *self, PyObject *args);
+  static PyObject *status(PyObject *self, PyObject *args);
+
+  //static void register_cleanup(PyObject *self, PyObject *args);
+} PyLevelDBIterator;
 
 // general exception object representing non-ok status from LevelDB APIs.
 static PyObject *leveldbError;
+
+PyObject *
+PyLevelDB::close(PyObject *self, PyObject *args) {
+  PyLevelDB* THIS = (PyLevelDB*)self;
+  if (THIS->db == NULL) {
+    PyErr_SetString(leveldbError, "closed database");
+    return NULL;
+  }
+  delete THIS->db;
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDB::closed(PyObject *self, PyObject *args) {
+  PyLevelDB* THIS = (PyLevelDB*)self;
+  if (THIS->db == NULL)
+    Py_RETURN_TRUE;
+  else
+    Py_RETURN_FALSE;
+}
 
 LongComparator comparator;
 
@@ -99,6 +147,10 @@ PyLevelDBIntHash::put(PyObject *self, PyObject *args) {
   int value_len;
   if (!PyArg_ParseTuple(args, "Lt#:put", &key, &value, &value_len))
     return NULL;
+  if (THIS->db == NULL) {
+    PyErr_SetString(leveldbError, "closed database");
+    return NULL;
+  }
   leveldb::Slice keyslice((char *)&key, sizeof(PY_LONG_LONG));
   leveldb::Slice valueslice(value, value_len);
   leveldb::Status sw;
@@ -121,6 +173,10 @@ PyLevelDBIntHash::get(PyObject *self, PyObject *args) {
   PY_LONG_LONG key;
   if (!PyArg_ParseTuple(args, "L:get", &key))
     return NULL;
+  if (THIS->db == NULL) {
+    PyErr_SetString(leveldbError, "closed database");
+    return NULL;
+  }
   leveldb::Slice keyslice((char *)&key, sizeof(PY_LONG_LONG));
   string value;
   leveldb::Status sr;
@@ -137,15 +193,83 @@ PyLevelDBIntHash::get(PyObject *self, PyObject *args) {
   return PyString_FromStringAndSize(value.data(), value.length());
 }
 
+PyObject *
+PyLevelDBIntHash::Delete(PyObject *self, PyObject *args) {
+  PyLevelDBIntHash *THIS = (PyLevelDBIntHash *)self;
+  // TODO: support "sync" WriteOption
+  PY_LONG_LONG key;
+  if (!PyArg_ParseTuple(args, "L:delete", &key))
+    return NULL;
+  if (THIS->db == NULL) {
+    PyErr_SetString(leveldbError, "closed database");
+    return NULL;
+  }
+  leveldb::Slice keyslice((char *)&key, sizeof(PY_LONG_LONG));
+  leveldb::Status status;
+  Py_BEGIN_ALLOW_THREADS
+  status = THIS->db->Delete(leveldb::WriteOptions(), keyslice);
+  Py_END_ALLOW_THREADS
+  if (!status.ok()) {
+    PyErr_SetString(leveldbError, status.ToString().c_str());
+    return NULL;
+  }
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDBIntHash::repair_db(PyObject *cls, PyObject *args, PyObject *kwds) {
+  static const char *kwlist[] = { "dbname",
+				  "block_cache_size", "block_size",
+				  "write_buffer_size", "max_open_files",
+				  "block_restart_interval",
+				  "create_if_missing", "error_if_exists",
+				  NULL };
+  char *dbname = NULL;
+  size_t cache_size = 0;
+  leveldb::Options options;
+  if (!PyArg_ParseTupleAndKeywords(args, kwds,
+				   "s|nnniibb", (char**)kwlist,
+				   &dbname,
+				   &cache_size, &options.block_size,
+				   &options.write_buffer_size,
+				   &options.max_open_files,
+				   &options.block_restart_interval,
+				   &options.create_if_missing,
+				   &options.error_if_exists))
+    return NULL;
+  if (cache_size > 0)
+    options.block_cache = leveldb::NewLRUCache(cache_size);
+  options.comparator = &comparator;
+  leveldb::Status status;
+  Py_BEGIN_ALLOW_THREADS
+  status = RepairDB(string(dbname), options);
+  Py_END_ALLOW_THREADS
+  if (!status.ok()) {
+    PyErr_SetString(leveldbError, status.ToString().c_str());
+    return NULL;
+  }
+  Py_RETURN_NONE;
+}
+
 static PyMethodDef PyLevelDBIntHash_methods[] = {
   {"put", (PyCFunction)PyLevelDBIntHash::put, METH_VARARGS,
    "updates value for the key"},
   {"get", (PyCFunction)PyLevelDBIntHash::get, METH_VARARGS,
    "gets value for the key. None if key does not exist"},
+  {"delete", (PyCFunction)PyLevelDBIntHash::Delete, METH_VARARGS,
+   "deletes entry for key. it is not an error to delete nonexistent key"},
+  {"new_iterator", (PyCFunction)PyLevelDBIntHash::new_iterator,
+   METH_KEYWORDS, "creates new iterator"},
+  {"close", (PyCFunction)PyLevelDBIntHash::close, METH_NOARGS,
+   "closes database"},
+  {"closed", (PyCFunction)PyLevelDBIntHash::closed, METH_NOARGS,
+   "returns True if database has been closed"},
+  {"repair_db", (PyCFunction)PyLevelDBIntHash::repair_db,
+   METH_KEYWORDS|METH_CLASS, "repairs a database. takes database directory and WriteOption as keywords"},
   {NULL}
 };
 
-static PyTypeObject PyLevelDBIntHashType {
+static PyTypeObject PyLevelDBIntHashType = {
   PyObject_HEAD_INIT(NULL)
   0, // ob_size
   "leveldb.IntHash", // tp_name
@@ -187,6 +311,176 @@ static PyTypeObject PyLevelDBIntHashType {
   0, /* tp_new */
 };
 
+void
+PyLevelDBIterator::dealloc(PyObject *self) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  if (THIS->it != NULL) {
+    delete THIS->it;
+    THIS->it = NULL;
+  }
+  self->ob_type->tp_free(self);
+}
+
+PyObject *
+PyLevelDBIterator::valid(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  PyObject *r = THIS->it->Valid() ? Py_True : Py_False;
+  Py_INCREF(r);
+  return r;
+}
+
+PyObject *
+PyLevelDBIterator::seek_to_first(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  THIS->it->SeekToFirst();
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDBIterator::seek_to_last(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  THIS->it->SeekToLast();
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDBIterator::seek(PyObject *self, PyObject *args) {
+  //PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  // TODO
+  return NULL;
+}
+
+PyObject *
+PyLevelDBIterator::next(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  THIS->it->Next();
+  // TODO error check?
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDBIterator::prev(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  THIS->it->Prev();
+  // TODO error check?
+  Py_RETURN_NONE;
+}
+
+PyObject *
+PyLevelDBIterator::key(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  // TODO this falls short for IntHash... we want to return int, not
+  // string.
+  leveldb::Slice k = THIS->it->key();
+  return PyString_FromStringAndSize(k.data(), k.size());
+}
+
+PyObject *
+PyLevelDBIterator::value(PyObject *self, PyObject *args) {
+  PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  leveldb::Slice v = THIS->it->value();
+  return PyString_FromStringAndSize(v.data(), v.size());
+}
+
+PyObject *
+PyLevelDBIterator::status(PyObject *self, PyObject *args) {
+  //PyLevelDBIterator *THIS = (PyLevelDBIterator*)self;
+  // TODO
+  return NULL;
+}
+
+static PyMethodDef PyLevelDBIterator_methods[] = {
+  {"valid", (PyCFunction)PyLevelDBIterator::valid, METH_NOARGS,
+   ""},
+  {"seek_to_first", (PyCFunction)PyLevelDBIterator::seek_to_first,
+   METH_NOARGS, ""},
+  //  {"seek_to_last", (PyCFunction)PyLevelDBIterator::seek_to_last,
+  //   METH_NOARGS, ""},
+  //  {"seek", (PyCFunction)PyLevelDBIterator::seek, METH_VARARGS,
+  //   ""},
+  {"next", (PyCFunction)PyLevelDBIterator::next, METH_NOARGS,
+   ""},
+  //  {"prev", (PyCFunction)PyLevelDBIterator::prev, METH_NOARGS,
+  //   ""},
+  {"key", (PyCFunction)PyLevelDBIterator::key, METH_NOARGS,
+   ""},
+  {"value", (PyCFunction)PyLevelDBIterator::value, METH_NOARGS,
+   ""},
+  //  {"status", (PyCFunction)PyLevelDBIterator::status, METH_NOARGS,
+  //   ""},
+  {NULL}
+};
+
+static PyTypeObject PyLevelDBIteratorType = {
+  PyObject_HEAD_INIT(NULL)
+  0, // ob_size
+  "leveldb.Iterator", // tp_name
+  sizeof(PyLevelDBIterator), // tp_basicsize
+  0, /* tp_itemsize */
+  (destructor)PyLevelDBIterator::dealloc, /* tp_dealloc */
+  0, /* tp_print */
+  0, /* tp_getattr */
+  0, /* tp_setattr */
+  0, /* tp_compare */
+  0, /* tp_repr */
+  0, /* tp_as_number */
+  0, /* tp_as_sequence */
+  0, /* tp_as_mapping */
+  0, /* tp_hash */
+  0, /* tp_call */
+  0, /* tp_str */
+  0, /* tp_getattro */
+  0, /* tp_setattro */
+  0, /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags */
+  "LevelDB Iterator", /* tp_doc */
+  0, /* tp_traverse */
+  0, /* tp_clear */
+  0, /* tp_richcompare */
+  0, /* tp_weaklistoffset */
+  0, /* tp_iter */
+  0, /* tp_iternext */
+  PyLevelDBIterator_methods, /* tp_methods */
+  0, /* tp_members */
+  0, /* tp_getset */
+  0, /* tp_base */
+  0, /* tp_dict */
+  0, /* tp_desc_get */
+  0, /* tp_desc_set */
+  0, /* tp_dictoffset */
+  0,  //(initproc)PyLevelDBIterator::init, /* tp_init */
+  0, /* tp_alloc */
+  0, /* tp_new */
+};
+
+PyObject *
+PyLevelDBIntHash::new_iterator(PyObject *self, PyObject *args,
+			       PyObject *kwds) {
+  PyLevelDBIntHash *THIS = (PyLevelDBIntHash *)self;
+  // TODO Snapshot not supported yet.
+  const char *kwlist[] = { "verify_checksms", "fill_cache", NULL };
+  leveldb::ReadOptions options;
+  if (!PyArg_ParseTupleAndKeywords(args, kwds, "|bb", (char**)kwlist,
+				   &options.verify_checksums,
+				   &options.fill_cache))
+    return NULL;
+  if (THIS->db == NULL) {
+    PyErr_SetString(leveldbError, "closed database");
+    return NULL;
+  }
+  leveldb::Iterator *iterator = NULL;
+  leveldb::Status status;
+  Py_BEGIN_ALLOW_THREADS
+  iterator = THIS->db->NewIterator(options);
+  Py_END_ALLOW_THREADS
+  if (iterator == NULL)
+    Py_RETURN_NONE;
+  PyLevelDBIterator* obj = PyObject_New(PyLevelDBIterator,
+					&PyLevelDBIteratorType);
+  obj->it = iterator;
+  return obj;
+}
+
 static PyMethodDef module_methods[] = {
   {NULL},
 };
@@ -197,6 +491,9 @@ initleveldb(void) {
   PyLevelDBIntHashType.tp_new = PyType_GenericNew;
   if (PyType_Ready(&PyLevelDBIntHashType) < 0)
     return;
+  PyLevelDBIteratorType.tp_new = PyType_GenericNew;
+  if (PyType_Ready(&PyLevelDBIteratorType) < 0)
+    return;
 
   PyObject *m = Py_InitModule3("leveldb", module_methods,
 			       "Python bridge for LevelDB");
@@ -205,9 +502,10 @@ initleveldb(void) {
   Py_INCREF(&PyLevelDBIntHashType);
   PyModule_AddObject(m, "IntHash", (PyObject*)&PyLevelDBIntHashType);
 
-  leveldbError = PyErr_NewException("leveldb.error", NULL, NULL);
+  Py_INCREF(&PyLevelDBIteratorType);
+  PyModule_AddObject(m, "Iterator", (PyObject*)&PyLevelDBIteratorType);
+
+  leveldbError = PyErr_NewException((char*)"leveldb.error", NULL, NULL);
   Py_INCREF(leveldbError);
   PyModule_AddObject(m, "error", leveldbError);
 }
-
-    
