f27fcea95f4900d6262bb350b888de4f2aa4c277
Kenji Nagahashi <kenji@archive.org>
2013-02-04 18:13:55 +0000

    add schedule script. fix old hardcoded seen directory path in seendump.py.

--- bin/seendump.py
+++ bin/seendump.py
@@ -4,13 +4,14 @@ LIBDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib'))
 if LIBDIR not in sys.path: sys.path.append(LIBDIR)
 import leveldb
 import struct
+import hqconfig
 
 if len(sys.argv) < 2:
   print >>sys.stderr, "specify job name"
   exit(1)
 job = sys.argv[1]
 
-h = leveldb.IntHash(os.path.join('/1/crawling/hq/seen', job))
+h = leveldb.IntHash(hqconfig.seendir(job))
 try:
   it = h.new_iterator()
   it.seek_to_first()
