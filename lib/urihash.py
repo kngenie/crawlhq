4e96a1ab8d8b9b7c5188a4810e4c0c5fd3f5a23f
Kenji Nagahashi <kenji@archive.org>
2013-10-04 23:02:57 +0000

    prevent urikey() from failing when uri contains non-ascii chars.

--- lib/urihash.py
+++ lib/urihash.py
@@ -6,5 +6,7 @@ except:
 _fp64 = FPGenerator(0xD74307D3FD3382DB, 64)
 
 def urikey(uri):
+    if isinstance(uri, unicode):
+        uri = uri.encode('utf-8')
     uhash = _fp64.sfp(uri)
     return uhash
