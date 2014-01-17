cebea9e3a744776dad3558f6bbd48fc9b4e31c0c
Kenji Nagahashi <kenji@archive.org>
2014-01-17 23:41:00 +0000

    install c++ extensions and files in ws, conf and static from within setup.py. add missed-out pymongo dependency.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import setup, find_packages, Extension
 
+import sys
 import os
+import glob
 # tentative - until all hq modules are moved under 'crawlhq' package.
 def list_modules():
     return [n[:-3] for n in os.listdir('lib') if n.endswith('.py')]
+
+ext_modules=[
+    Extension('cfpgenerator', ['cext/pythonif.cpp', 'cext/fpgenerator.cpp'])
+    ]
+# XXX linux only
+if os.path.isfile('/usr/lib/libleveldb.a'):
+    ext_modules.append(Extension('leveldb', ['cext/leveldb.cpp'],
+                                 libraries=['leveldb']))
 
 setup(
     name="crawlhq",
@@ -18,6 +29,8 @@ setup(
     package_dir={'':'lib'},
     packages=find_packages('lib'),
     py_modules=list_modules(),
+    ext_modules=ext_modules,
+    zip_safe=False,
     entry_points={
         'console_scripts':[
             'shuffle = shuffle:main'
@@ -30,10 +43,24 @@ setup(
         'python-snappy>=0.3.2',
         'web.py>=0.34',
         'kazoo>=1.3',
+        'pymongo>=2.4.1'
+        ],
+    tests_require=[
+        'pytest'
         ],
     # TODO: use entry_points
     scripts=[
         'bin/processinq-sa',
         'bin/schedule',
+        ],
+    data_files=[
+        (os.path.join(sys.prefix, d), ff) for d, ff in
+        {
+        'conf': ['conf/apache2.inc'],
+        'ws': glob.glob('ws/*.py'),
+        'ws/t': glob.glob('ws/t/*.html'),
+        'static': glob.glob('static/*.css')+glob.glob('static/*.js'),
+        'static/img': glob.glob('static/img/*')
+        }.items()
         ]
 )
